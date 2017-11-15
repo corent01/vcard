07191323e59003bf55fd6abb1c55846772347a07
corent1 <[corentinwarin@gmail.com]>
2017-11-15 16:33:26 +0100

    vcard final 2.2

--- testmail.php
+++ testmail.php
@@ -1,66 +1,20 @@
 <?php
-
-//expéditeur du message
-
-$from = "corentinwarin@gmail.com";
-
-
-//destinataire du message
-
-//ici on fais correspondre  l’expéditeur, et destinataire pour le test
-
-$to = $from;
-
-
-//sujet du message
-
-$subject = "Test email";
-
-
-//corps du message
-
-$message = "Ceci est un message pour le test.\n";
-
-
-$headers = "From:" .$from. "\r\n" ;
-
-$headers .='Reply-To:'.$from."\r\n";
-
-$headers .='Content-Type: text/plain; charset="iso-8859-1"'."\r\n";
-
-$headers .='Content-Transfer-Encoding: 7bit'."\r\n";
-
-
-//on récupère la variable $_POST, on l’insère dans le message
-
-
-foreach ($_POST as $cle => $valeur) {
-
-
-// ici on peut vérifier si $cle ="submit" et l'enlever du message cela correspond au bouton envoyer, donc inutile
-
-// si on peut aussi a partir d'ici ré-initialiser l'objet du message
-
-
-$message.= "$cle : $valeur.\n";
-
-
+$destinataire = $_POST['email'];
+// Pour les champs $expediteur / $copie / $destinataire, séparer par une virgule s'il y a plusieurs adresses
+$expediteur = 'corentinwarin@gmail.com';
+$objet = $_POST['subject'];
+$headers  = 'MIME-Version: 1.0' . "\n"; // Version MIME
+$headers .= 'Content-type: text/html; charset=ISO-8859-1'."\n"; // l'en-tete Content-type pour le format HTML
+$headers .= 'Reply-To: '.$expediteur."\n"; // Mail de reponse
+$headers .= 'From: "Nom_de_expediteur"<'.$expediteur.'>'."\n"; // Expediteur
+$headers .= 'Delivered-to: '.$destinataire."\n"; // Destinataire
+$message = '<div style="width: 100%; text-align: center; font-weight: bold"> Bonjour'.$_POST['name'].'! \n'.$_POST['message'].'</div>';
+if (mail($destinataire, $objet, $message, $headers)) // Envoi du message
+{
+    echo 'Votre message a bien été envoyé ';
 }
-
- if(mail($to,$subject,$message,$headers))
-
-    {
-
-          echo 'Le message a bien été envoyé';
-
-    }
-
-    else
-
-    {
-
-          echo 'Le message n\'a pu être envoyé';
-
-    } //*******************/
-
+else // Non envoyé
+{
+    echo "Votre message n'a pas pu être envoyé";
+}
 ?>
